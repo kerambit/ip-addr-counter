0691dcd32166dd41c58074321a8298af9287cc97
kerambitls <126450593+kerambitls@users.noreply.github.com>
2025-04-21 11:37:37 +0400

    add parsing cmd args

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+ "flag"
+ "fmt"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+    var(
+        filePath string
+        allowParallel bool
+    )
+
+    flag.StringVar(&filePath, "path", "", "Path to the file")
+    flag.BoolVar(&allowParallel, "allowParallel", false, "Allow parallel processing")
+    flag.Parse()
+
+	fmt.Println("path is:", filePath)
+	fmt.Println("allowParallel is:", allowParallel)
+
+	printMemUsage()
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", m.Alloc/1024/1024)
+	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc/1024/1024)
+	fmt.Printf("\tSys = %v MiB", m.Sys/1024/1024)
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
 }
